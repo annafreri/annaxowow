fe534bfd13a99289353766c30976ecac0efa8c0d
annafreri <140950208+annafreri@users.noreply.github.com>
2025-02-26 11:30:18 +0100

    feat(frontend): add all necessary components and links in auth page

--- src/components/pages/AuthPage.tsx
+++ src/components/pages/AuthPage.tsx
@@ -1,21 +1,67 @@
+import { Input } from "../ui/input"
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 
 export default function AuthPage() {
   return (
-    <div>
-      <h1>Authentication Page</h1>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="email" />
+    <div className="h-screen w-full flex items-center justify-center text-zinc-400">
+      <div className="max-w-md flex flex-col gap-8 bg-zinc-800 rounded-2xl py-12 px-16 ">
+        <div className="text-center flex flex-col gap-4 items-center">
+          <p className="text-sm font-semibold tracking-wide font-mono uppercase">
+            Welcome to
+          </p>
+          <h1 className="text-6xl font-bold text-purple-400">
+            Wizkid Manager
+            <sup className="text-2xl pl-1">&#174;</sup>
+          </h1>
+          <p className="bg-purple-400 w-fit rounded-full px-2 text-zinc-950 text-sm -rotate-6">
+            2000
+          </p>
         </div>
+
         <div>
-          <label>Password:</label>
-          <input type="password" />
+
+          <form className="flex flex-col gap-6">
+
+            <div
+              className="flex flex-col gap-3">
+              <Input
+                type="email"
+                placeholder="Email"
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <Link
+              to="/dashboard"
+              className="w-full"
+            >
+              <Button
+                type="submit"
+                variant="secondary"
+                size="default"
+                className="w-full"
+              >
+                Login
+              </Button>
+            </Link>
+
+          </form>
+
+          <div className="flex justify-center mt-4">
+            <Button
+              type="button"
+              variant="link">
+              Forgot Password
+            </Button>
+          </div>
+
         </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
-
